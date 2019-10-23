e46aded61a20d53b808092744b38bf6027d368b1
kaarthik-raja <kaarthikrajamv@gmail.com>
2019-10-24 00:59:04 +0530

    .

--- YOLO/src/detector.c
+++ YOLO/src/detector.c
@@ -445,6 +445,8 @@ void validate_detector(char *datacfg, char *cfgfile, char *weightfile, char *out
     if (weightfile) {
         load_weights(&net, weightfile);
     }
+    printf("1__________________________________");
+
     //set_batch_network(&net, 1);
     fprintf(stderr, "Learning Rate: %g, Momentum: %g, Decay: %g\n", net.learning_rate, net.momentum, net.decay);
     srand(time(0));
@@ -461,12 +463,16 @@ void validate_detector(char *datacfg, char *cfgfile, char *weightfile, char *out
     FILE **fps = 0;
     int coco = 0;
     int imagenet = 0;
+    printf("__________________________________");
+
     if (0 == strcmp(type, "coco")) {
         if (!outfile) outfile = "coco_results";
         snprintf(buff, 1024, "%s/%s.json", prefix, outfile);
         fp = fopen(buff, "w");
         fprintf(fp, "[\n");
         coco = 1;
+	    printf("coco__________________________________");
+
     }
     else if (0 == strcmp(type, "imagenet")) {
         if (!outfile) outfile = "imagenet-detection";
@@ -514,6 +520,8 @@ void validate_detector(char *datacfg, char *cfgfile, char *weightfile, char *out
         thr[t] = load_data_in_thread(args);
     }
     time_t start = time(0);
+    printf("for__________________________________%d - %d" , m , nthreads);
+
     for (i = nthreads; i < m + nthreads; i += nthreads) {
         fprintf(stderr, "%d\n", i);
         for (t = 0; t < nthreads && i + t - nthreads < m; ++t) {
@@ -798,7 +806,7 @@ float validate_detector_map(char *datacfg, char *cfgfile, char *weightfile, floa
             replace_image_to_label(path, labelpath);
             int num_labels = 0;
             box_label *truth = read_boxes(labelpath, &num_labels);
-            int i, j;
+            int j;
             for (j = 0; j < num_labels; ++j) {
                 truth_classes_count[truth[j].id]++;
             }
@@ -818,6 +826,7 @@ float validate_detector_map(char *datacfg, char *cfgfile, char *weightfile, floa
 
             const int checkpoint_detections_count = detections_count;
 
+            int i;
             for (i = 0; i < nboxes; ++i) {
 
                 int class_id;
@@ -1487,11 +1496,7 @@ void run_detector(int argc, char **argv)
     else if (0 == strcmp(argv[2], "train")) train_detector(datacfg, cfg, weights, gpus, ngpus, clear, dont_show, calc_map, mjpeg_port, show_imgs);
     else if (0 == strcmp(argv[2], "valid")) validate_detector(datacfg, cfg, weights, outfile);
     else if (0 == strcmp(argv[2], "recall")) validate_detector_recall(datacfg, cfg, weights);
-    else if (0 == strcmp(argv[2], "map")) {
-            validate_detector_map(datacfg, cfg, weights, thresh, 0.9, map_points, letter_box, NULL);
-            validate_detector_map(datacfg, cfg, weights, thresh, 0.75, map_points, letter_box, NULL);
-            validate_detector_map(datacfg, cfg, weights, thresh, 0.5, map_points, letter_box, NULL);
-            }
+    else if (0 == strcmp(argv[2], "map")) validate_detector_map(datacfg, cfg, weights, thresh, iou_thresh, map_points, letter_box, NULL);
     else if (0 == strcmp(argv[2], "calc_anchors")) calc_anchors(datacfg, num_of_clusters, width, height, show);
     else if (0 == strcmp(argv[2], "demo")) {
         list *options = read_data_cfg(datacfg);
